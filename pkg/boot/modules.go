dc1ba36be5c95b3bb33de980bb0a8f2a781dc1b8
Florin Peter <github@florin-peter.de>
2018-06-14 21:33:28 +0200

    Update modules.go

--- pkg/boot/modules.go
+++ pkg/boot/modules.go
@@ -1,33 +1,32 @@
 package boot
 
-// nolint: lll
 import (
 	"fmt"
 	"time"
 
-	aciSDK "github.com/Azure/azure-sdk-for-go/services/containerinstance/mgmt/2017-08-01-preview/containerinstance"
-	cosmosSDK "github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2015-04-08/documentdb"
-	eventHubSDK "github.com/Azure/azure-sdk-for-go/services/eventhub/mgmt/2017-04-01/eventhub"
-	keyVaultSDK "github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2016-10-01/keyvault"
-	mysqlSDK "github.com/Azure/azure-sdk-for-go/services/mysql/mgmt/2017-04-30-preview/mysql"
-	postgresSDK "github.com/Azure/azure-sdk-for-go/services/postgresql/mgmt/2017-04-30-preview/postgresql"
-	redisSDK "github.com/Azure/azure-sdk-for-go/services/redis/mgmt/2017-10-01/redis"
-	resourcesSDK "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2017-05-10/resources"
-	searchSDK "github.com/Azure/azure-sdk-for-go/services/search/mgmt/2015-08-19/search"
-	servicebusSDK "github.com/Azure/azure-sdk-for-go/services/servicebus/mgmt/2017-04-01/servicebus"
-	sqlSDK "github.com/Azure/azure-sdk-for-go/services/sql/mgmt/2017-03-01-preview/sql"
-	storageSDK "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2017-10-01/storage"
+	aciSDK "github.com/Azure/azure-sdk-for-go/services/containerinstance/mgmt/2017-08-01-preview/containerinstance" // nolint: lll
+	cosmosSDK "github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2015-04-08/documentdb"                     // nolint: lll
+	eventHubSDK "github.com/Azure/azure-sdk-for-go/services/eventhub/mgmt/2017-04-01/eventhub"                      // nolint: lll
+	keyVaultSDK "github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2016-10-01/keyvault"                      // nolint: lll
+	mysqlSDK "github.com/Azure/azure-sdk-for-go/services/mysql/mgmt/2017-04-30-preview/mysql"                       // nolint: lll
+	postgresSDK "github.com/Azure/azure-sdk-for-go/services/postgresql/mgmt/2017-04-30-preview/postgresql"          // nolint: lll
+	redisSDK "github.com/Azure/azure-sdk-for-go/services/redis/mgmt/2017-10-01/redis"                               // nolint: lll
+	resourcesSDK "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2017-05-10/resources"                   // nolint: lll
+	searchSDK "github.com/Azure/azure-sdk-for-go/services/search/mgmt/2015-08-19/search"                            // nolint: lll
+	servicebusSDK "github.com/Azure/azure-sdk-for-go/services/servicebus/mgmt/2017-04-01/servicebus"                // nolint: lll
+	sqlSDK "github.com/Azure/azure-sdk-for-go/services/sql/mgmt/2017-03-01-preview/sql"                             // nolint: lll
+	storageSDK "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2017-10-01/storage"                         // nolint: lll
 	"github.com/Azure/go-autorest/autorest"
- 	"github.com/Azure/open-service-broker-azure/pkg/azure"
- 	"github.com/Azure/open-service-broker-azure/pkg/azure/arm"
- 	"github.com/Azure/open-service-broker-azure/pkg/service"
+	"github.com/Azure/open-service-broker-azure/pkg/azure"
+	"github.com/Azure/open-service-broker-azure/pkg/azure/arm"
+	"github.com/Azure/open-service-broker-azure/pkg/service"
 	"github.com/Azure/open-service-broker-azure/pkg/services/aci"
 	"github.com/Azure/open-service-broker-azure/pkg/services/cosmosdb"
 	"github.com/Azure/open-service-broker-azure/pkg/services/eventhubs"
 	"github.com/Azure/open-service-broker-azure/pkg/services/keyvault"
- 	"github.com/Azure/open-service-broker-azure/pkg/services/mssql"
- 	"github.com/Azure/open-service-broker-azure/pkg/services/mysql"
- 	"github.com/Azure/open-service-broker-azure/pkg/services/postgresql"
+	"github.com/Azure/open-service-broker-azure/pkg/services/mssql"
+	"github.com/Azure/open-service-broker-azure/pkg/services/mysql"
+	"github.com/Azure/open-service-broker-azure/pkg/services/postgresql"
 	"github.com/Azure/open-service-broker-azure/pkg/services/rediscache"
 	"github.com/Azure/open-service-broker-azure/pkg/services/search"
 	"github.com/Azure/open-service-broker-azure/pkg/services/servicebus"
@@ -71,34 +70,33 @@ func getModules(
 	)
 
 	aciClient := aciSDK.NewContainerGroupsClientWithBaseURI(
-	 	azureConfig.Environment.ResourceManagerEndpoint,
-	 	azureSubscriptionID,
+		azureConfig.Environment.ResourceManagerEndpoint,
+		azureSubscriptionID,
 	)
 	aciClient.Authorizer = authorizer
 	aciClient.UserAgent = getUserAgent(aciClient.Client)
 
-	// cosmosdbAccountsClient := cosmosSDK.NewDatabaseAccountsClientWithBaseURI(
-	// 	azureConfig.Environment.ResourceManagerEndpoint,
-	// 	azureSubscriptionID,
-	// )
-	// cosmosdbAccountsClient.Authorizer = authorizer
-	// cosmosdbAccountsClient.UserAgent =
-	// 	getUserAgent(cosmosdbAccountsClient.Client)
+	cosmosdbAccountsClient := cosmosSDK.NewDatabaseAccountsClientWithBaseURI(
+		azureConfig.Environment.ResourceManagerEndpoint,
+		azureSubscriptionID,
+	)
+	cosmosdbAccountsClient.Authorizer = authorizer
+	cosmosdbAccountsClient.UserAgent = getUserAgent(cosmosdbAccountsClient.Client)
 
-	// eventHubNamespacesClient := eventHubSDK.NewNamespacesClientWithBaseURI(
-	// 	azureConfig.Environment.ResourceManagerEndpoint,
-	// 	azureSubscriptionID,
-	// )
-	// eventHubNamespacesClient.Authorizer = authorizer
-	// eventHubNamespacesClient.UserAgent =
-	// 	getUserAgent(eventHubNamespacesClient.Client)
+	eventHubNamespacesClient := eventHubSDK.NewNamespacesClientWithBaseURI(
+		azureConfig.Environment.ResourceManagerEndpoint,
+		azureSubscriptionID,
+	)
+	eventHubNamespacesClient.Authorizer = authorizer
+	eventHubNamespacesClient.UserAgent =
+		getUserAgent(eventHubNamespacesClient.Client)
 
-	// keyVaultsClient := keyVaultSDK.NewVaultsClientWithBaseURI(
-	// 	azureConfig.Environment.ResourceManagerEndpoint,
-	// 	azureSubscriptionID,
-	// )
-	// keyVaultsClient.Authorizer = authorizer
-	// keyVaultsClient.UserAgent = getUserAgent(keyVaultsClient.Client)
+	keyVaultsClient := keyVaultSDK.NewVaultsClientWithBaseURI(
+		azureConfig.Environment.ResourceManagerEndpoint,
+		azureSubscriptionID,
+	)
+	keyVaultsClient.Authorizer = authorizer
+	keyVaultsClient.UserAgent = getUserAgent(keyVaultsClient.Client)
 
 	mysqlCheckNameAvailabilityClient :=
 		mysqlSDK.NewCheckNameAvailabilityClientWithBaseURI(
